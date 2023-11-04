bf2065a3e0885e96c4ddf4598640f0396e76024d
Heliam Belle <hbelle@3A2.42angouleme.fr>
2023-11-04 15:36:11 +0100

    automatic save push

--- Projet/libft/ft_lstmap.c
+++ Projet/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: hbelle <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:17:33 by hbelle            #+#    #+#             */
-/*   Updated: 2023/11/04 13:55:38 by hbelle           ###   ########.fr       */
+/*   Updated: 2023/11/04 15:21:36 by hbelle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,16 +18,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (!lst)
 		return (NULL);
-	while (lst != NULL)
-		new = ft_lstnew(f(lst->content));
-	ptr = new;
-	lst = lst->next;
-	while (lst != NULL)
+	if (lst != NULL)
 	{
 		new = ft_lstnew(f(lst->content));
-		if (!new)
-			ft_lstclear(&ptr, del);
-		ft_lstadd_back(&ptr, new);
+		ptr = new;
+		while (lst->next != NULL)
+		{
+			lst = lst->next;
+			new = ft_lstnew(f(lst->content));
+			if (!new)
+				ft_lstclear(&ptr, del);
+			ft_lstadd_back(&ptr, new);
+		}
+		return (new);
 	}
-	return (new);
+	return (NULL);
 }
